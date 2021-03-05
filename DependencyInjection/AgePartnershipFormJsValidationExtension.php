9ff3a2577f3aa402d515e910b5d440e634ba88fc
Harry Brook-Dobson <harry.brook-dobson@agepartnership.com>
2021-03-05 14:35:18 +0000

    Load files logically rather than explicitly

--- DependencyInjection/AgePartnershipFormJsValidationExtension.php
+++ DependencyInjection/AgePartnershipFormJsValidationExtension.php
@@ -22,7 +22,7 @@ class AgePartnershipFormJsValidationExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new Loader\YamlFileLoader($container, new FileLocator('@FormJsValidationBundle/Resources/config'));
         $loader->load('services.yml');
     }
 }
