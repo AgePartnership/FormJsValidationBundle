723f2867e81ea8c87e0dc0f5e1a6cfb78290ec79
Harry Brook-Dobson <harry.brook-dobson@agepartnership.com>
2021-03-29 13:35:45 +0100

    Config symfony in a 5y way

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,8 +17,7 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('age_partnership_form_js_validation');
+        $treeBuilder = new TreeBuilder('age_partnership_form_js_validation');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
